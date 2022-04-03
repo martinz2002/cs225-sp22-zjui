2f70117776ec94ab926c99f751d1978704838ec4
inwinleo <inwin_leo@qq.com>
2022-04-03 18:01:09 +0800

    fibo structured

--- computings/compass1/fiboheap.cpp
+++ computings/compass1/fiboheap.cpp
@@ -1,2 +1,66 @@
 #include "headers/fiboheap.h";
 
+template <class dataregionT, class keyT>
+heapnodes<dataregionT, keyT>::heapnodes()
+{
+}
+
+template <class dataregionT, class keyT>
+void heapnodes<dataregionT, keyT>::add_brother(heapnodes<dataregionT, keyT> *ptr)
+{
+}
+
+template <class dataregionT, class keyT>
+void heapnodes<dataregionT, keyT>::add_children(heapnodes<dataregionT, keyT> *ptr)
+{
+}
+
+template <class dataregionT, class keyT>
+void heapnodes<dataregionT, keyT>::to_root()
+{
+}
+
+template <class dataregionT, class keyT>
+void heapnodes<dataregionT, keyT>::self_delete()
+{
+}
+
+template <class dataregionT, class keyT>
+void heapnodes<dataregionT, keyT>::change_value(keyT new_key)
+{
+}
+
+template <class dataregionT, class keyT>
+fiboheap<dataregionT, keyT>::fiboheap()
+{
+}
+
+template <class dataregionT, class keyT>
+dataregionT fiboheap<dataregionT, keyT>::getmin()
+{
+}
+
+template <class dataregionT, class keyT>
+dataregionT fiboheap<dataregionT, keyT>::popmin()
+{
+}
+
+template <class dataregionT, class keyT>
+bool fiboheap<dataregionT, keyT>::merge(fiboheap *heap_pointer)
+{
+}
+
+template <class dataregionT, class keyT>
+bool fiboheap<dataregionT, keyT>::delete_node(heapnodes<dataregionT, keyT> *nodepointer)
+{
+}
+
+template <class dataregionT, class keyT>
+bool fiboheap<dataregionT, keyT>::change_value(heapnodes<dataregionT, keyT> *nodepointer, keyT key)
+{
+}
+
+template <class dataregionT, class keyT>
+heapnodes<dataregionT, keyT> *fiboheap<dataregionT, keyT>::insert_nodes(dataregionT data, keyT key)
+{
+}
--- computings/compass1/headers/fiboheap.h
+++ computings/compass1/headers/fiboheap.h
@@ -1,5 +1,31 @@
 #include <bits/stdc++.h>
 
+
+
+template <class dataregionT, class keyT>
+class heapnodes
+{
+public:
+    heapnodes();
+    ~heapnodes();
+
+public:
+    void add_brother(heapnodes<dataregionT,keyT> *ptr);
+    void add_children(heapnodes<dataregionT,keyT> *ptr);
+    void to_root();
+    void self_delete();
+    void change_value(keyT new_key);
+
+private:
+    int64_t degree;
+    heapnodes<dataregionT,keyT> *parent;
+    heapnodes<dataregionT,keyT> *children;
+    heapnodes<dataregionT,keyT> *left;
+    heapnodes<dataregionT,keyT> *right;
+    bool marked;
+    dataregionT dataregion;
+    keyT key;
+};
 template <class dataregionT, class keyT>
 class fiboheap
 {
@@ -8,40 +34,17 @@ public:
     ~fiboheap();
 
 public:
-    dataregionT min();
+    dataregionT getmin();
     dataregionT popmin();
-    bool merge(fiboheap *heap_pointer);
-    bool delete_node(heapnodes* nodepointer);
-    bool change_value(heapnodes* nodepointer, keyT key);
-    heapnodes* insert_nodes(dataregionT data, keyT key);
-    bool to_root(heapnodes* nodepointer);
+    bool merge(fiboheap<dataregionT,keyT> *heap_pointer);
+    bool delete_node(heapnodes<dataregionT,keyT> *nodepointer);
+    bool change_value(heapnodes<dataregionT,keyT> *nodepointer, keyT new_key);
+    heapnodes<dataregionT,keyT>* insert_nodes(dataregionT data, keyT key);
+    bool to_root(heapnodes<dataregionT,keyT> *nodepointer);
 
 private:
     void maintain_heap();
     heapnodes<dataregionT, keyT> *minnode;
     int64_t maxdegree;
     heapnodes<dataregionT, keyT> *nodes;
-};
-
-template <class dataregionT, class keyT>
-class heapnodes
-{
-public:
-    heapnodes();
-    ~heapnodes();
-public:
-    void add_brother();
-    void add_children();
-    void to_root();
-    void self_delete();
-    void change_value();
-private:
-    int64_t degree;
-    heapnodes *parent;
-    heapnodes *children;
-    heapnodes *left;
-    heapnodes *right;
-    bool marked;
-    dataregionT dataregion;
-    keyT key;
 };
