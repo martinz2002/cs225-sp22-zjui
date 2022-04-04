8619fa18439095576816548bbc34837d86470d3c
inwinleo <inwin_leo@qq.com>
2022-04-04 15:45:45 +0800

    hahahahhahaha

--- computings/compass1/headers/profile.h
+++ computings/compass1/headers/profile.h
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-struct profile
+struct personal_profile
 {
 public:
-    profile();
-    ~profile();
+    personal_profile();
+    ~personal_profile();
 
 public:
     char *address = new char[256];
@@ -19,7 +19,36 @@ public:
     int64_t registrationdate;
 
 public:
-    profile *next_node;
-    profile *previous_node;
+    personal_profile *next_node;
+    personal_profile *previous_node;
 };
 
+struct registration_profile
+{
+public:
+    registration_profile();
+    ~registration_profile();
+
+public:
+    int64_t ID;
+    int64_t *vaccination_sequence;
+    int x_coordinate;
+    int y_coordinate;
+};
+
+struct inoculate_profile
+{
+public:
+    inoculate_profile();
+    ~inoculate_profile();
+
+public:
+    int64_t ID;
+    int x_coordinate;
+    int y_coordinate;
+    int daily_processnum;
+
+public:
+    inoculate_profile *next_node;
+    inoculate_profile *previous_node;
+};
