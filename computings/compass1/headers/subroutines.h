c5909c84720b786253839743b1c214eb454fc13f
inwinleo <inwin_leo@qq.com>
2022-04-04 18:26:36 +0800

    传

--- computings/compass1/headers/subroutines.h
+++ computings/compass1/headers/subroutines.h
@@ -8,7 +8,7 @@ static void reg_reg(int64_t x,int64_t y);
 
 static void reg_ino(int64_t x,int64_t y,int cap);
 
-static void add_profile(string address, string phone, string WeChat, string email, int risk, int64_t ID, int profession, int agegroup, int64_t birthdate, int64_t registrationdate, int64_t RegID);
+static void add_profile(string address, string phone, string WeChat, string email, int risk, int64_t ID, int profession, int agegroup, int64_t birthdate, int64_t RegID);
 
 static void calc_reg_dist();
 
--- computings/compass1/subroutines.cpp
+++ computings/compass1/subroutines.cpp
@@ -1,11 +1,26 @@
 #include "headers/subroutines.h"
 
+static int64_t date = 0;
 int64_t num_reg = 0;
 int64_t num_ino = 0;
 int64_t max_num_reg = 20;
 int64_t max_num_ino = 20;
 registration_profile **reg_pro = new registration_profile *[20];
 inoculate_profile **ino_pro = new inoculate_profile *[20];
+
+personal_profile *first_personal_file;
+personal_profile *last_personal_file;
+
+int64_t *dist;
+
+using std::pow;
+using std::sort;
+using std::sqrt;
+
+bool compare(int64_t num1, int64_t num2)
+{
+    return dist[num1] > dist[num2];
+}
 
 void alloc_for_reg()
 {
@@ -35,6 +50,7 @@ static void reg_reg(int64_t x, int64_t y)
     reg_pro[num_reg]->x_coordinate = x;
     reg_pro[num_reg]->y_coordinate = y;
     num_reg++;
+    reg_pro[num_reg]->ID = num_reg;
     if (num_reg >= max_num_reg)
         alloc_for_reg();
 }
@@ -45,13 +61,38 @@ static void reg_ino(int64_t x, int64_t y, int cap)
     ino_pro[num_ino]->x_coordinate = x;
     ino_pro[num_ino]->y_coordinate = y;
     num_ino++;
+    ino_pro[num_ino]->ID = num_ino;
     if (num_ino >= max_num_ino)
         alloc_for_ino();
 }
 
-static void add_profile(string address, string phone, string WeChat, string email, int risk, int64_t ID, int profession, int agegroup, int64_t birthdate, int64_t registrationdate, int64_t RegID) {}
-
-static void calc_reg_dist() {}
+static void calc_reg_dist()
+{
+    dist = new int64_t[num_ino];
+    for (int _ = 0; _ < num_reg; _++)
+    {
+        reg_pro[_]->vaccination_sequence = new int64_t[num_ino];
+        for (int i = 0; i < num_ino; i++)
+        {
+            dist[i] = sqrt(pow(ino_pro[i]->x_coordinate - reg_pro[_]->x_coordinate, 2) + pow(ino_pro[i]->y_coordinate - reg_pro[_]->y_coordinate, 2));
+            reg_pro[_]->vaccination_sequence[i] = ino_pro[i]->ID;
+        }
+        sort(reg_pro[_]->vaccination_sequence, reg_pro[_]->vaccination_sequence + num_ino, compare);
+    }
+}
+static void add_profile(string address, string phone, string WeChat, string email, int risk, int64_t ID, int profession, int agegroup, int64_t birthdate, int64_t RegID)
+{
+    if (first_personal_file == NULL)
+    {
+        first_personal_file = newprofile(first_personal_file, address, phone, WeChat, email, risk, ID, profession, agegroup, birthdate, date, RegID);
+        last_personal_file = first_personal_file;
+    }
+    else
+    {
+        last_personal_file = newprofile(first_personal_file, address, phone, WeChat, email, risk, ID, profession, agegroup, birthdate, date, RegID);
+    }
+    registration_sequence_calculation(last_personal_file, reg_pro[RegID]);
+}
 
 static void report() {}
 
