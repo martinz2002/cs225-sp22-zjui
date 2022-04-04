e3bbd8ef12154e22f4cccf8d83e69fb36533acb0
inwinleo <inwin_leo@qq.com>
2022-04-04 17:31:06 +0800

    Update subroutines.cpp

--- computings/compass1/subroutines.cpp
+++ computings/compass1/subroutines.cpp
@@ -4,13 +4,13 @@ int64_t num_reg = 0;
 int64_t num_ino = 0;
 int64_t max_num_reg = 20;
 int64_t max_num_ino = 20;
-registration_profile *reg_pro = new registration_profile[20];
-inoculate_profile *ino_pro = new inoculate_profile[20];
+registration_profile **reg_pro = new registration_profile *[20];
+inoculate_profile **ino_pro = new inoculate_profile *[20];
 
 void alloc_for_reg()
 {
     max_num_reg *= 2;
-    registration_profile *new_reg = new registration_profile[max_num_reg];
+    registration_profile **new_reg = new registration_profile *[max_num_reg];
     for (int _ = 0; _ < num_reg; _++)
     {
         new_reg[_] = reg_pro[_];
@@ -20,11 +20,23 @@ void alloc_for_reg()
 
 void alloc_for_ino()
 {
-    
+    max_num_ino *= 2;
+    inoculate_profile **new_ino = new inoculate_profile *[max_num_ino];
+    for (int _ = 0; _ < num_ino; _++)
+    {
+        new_ino[_] = ino_pro[_];
+    }
+    ino_pro = new_ino;
 }
 
 static void reg_reg(int64_t x, int64_t y)
 {
+    reg_pro[num_reg] = new registration_profile;
+    reg_pro[num_reg]->x_coordinate = x;
+    reg_pro[num_reg]->y_coordinate = y;
+    num_reg++;
+    if (num_reg >= max_num_reg)
+        alloc_for_reg();
 }
 
 static void reg_ino(int64_t x, int64_t y, int cap) {}
