fd92d6adf2d45c0118b263356c35ace1d50f4b3e
Tiantian Zhong <bcsyztt@outlook.com>
2022-04-08 21:12:06 +0800

    fuck

--- computings/compass1/runsys.cpp
+++ computings/compass1/runsys.cpp
@@ -29,21 +29,29 @@ int main()
     calc_reg_dist(); // Calculate the distance between registration points and inoculation points
 
     // import profiles
-    static CDate *next_profile_date = new CDate;  // date of next profile
-    static CDate *next_withdraw_date = new CDate; // date of next withdrawal
+    static CDate *next_profile_date = new CDate;         // date of next profile
+    static CDate *next_withdraw_date = new CDate;        // date of next withdrawal
+    static CDate *next_modify_profiles_date = new CDate; // date of next modification of profiles
+    static CDate *next_priority_date = new CDate;        // date of importing next priority letter
 
-    string name, address, phone, WeChat, email, birthdate, temp_date;
+    string name, address, phone, WeChat, email, birthdate;
+    string temp_date;                 // Input temp when reading profile date
+    string temp_date_for_withdraw;    // input temp when reading withdrawal/cancellation date
+    string temp_date_priority_letter; // input temp when reading priority letter date
+    string temp_date_modification;    // input temp when reading profile modification date
     int risk, profession;
     int64_t ID, RegID;
+    int op_withdraw; // 0 = cancel withdraw; 1 = withdraw
 
-    ifstream profile_stream, withdraw_stream;
+    ifstream profile_stream, withdraw_stream, priority_letter_stream, modify_profile_stream;
     profile_stream.open("data/profiles.entries", ios::in);
     withdraw_stream.open("data/withdraw.entries", ios::in);
+    priority_letter_stream.open("data/priority_letter.entries", ios::in);
+    modify_profile_stream.open("data/modify_profile.entries", ios::in);
 
     // Get the first date of entry of profiles and withdrawals
     profile_stream >> temp_date;
     next_profile_date->set(temp_date);
-    string temp_date_for_withdraw;
     withdraw_stream >> temp_date_for_withdraw;
     next_withdraw_date->set(temp_date_for_withdraw);
     // Iterate through profile entries and withdrawals and record them
@@ -58,13 +66,33 @@ int main()
             profile_stream >> temp_date;
             next_profile_date->set(temp_date);
         }
-        while (next_withdraw_date <= date && temp_date_for_withdraw != "EOF") // if today we have to perform a withdrawal
+        while ((*next_withdraw_date) <= (*date) && temp_date_for_withdraw != "EOF") // if today we have to perform a withdrawal/cancellation
         {
             withdraw_stream >> ID;
             withdraw(ID);
+            withdraw_stream >> op_withdraw;
             withdraw_stream >> temp_date_for_withdraw;
             next_withdraw_date->set(temp_date_for_withdraw);
+            if (op_withdraw == 0)
+            {
+                // cancel withdraw
+                cancel_withdraw(ID);
+            }
+            if (op_withdraw == 1)
+            {
+                // withdraw
+                withdraw(ID);
+            }
+            else
+            {
+                // illegal withdraw operation code
+                cout << "[Error] Illegal entry at date:";
+                (*date).print();
+                cout << "    ID: " << ID << endl;
+            }
         }
-        next_day(); // go to next day
+        while ()
+
+            next_day(); // go to next day
     }
 }
